ee765c05f1b2127496ff69b115b7f1e4a1d88f2b
fitraelbi <66520845+fitraelbi@users.noreply.github.com>
2025-10-08 08:27:48 +0700

    fix: change node version

--- instrument.js
+++ instrument.js
@@ -3,15 +3,26 @@ const { nodeProfilingIntegration } = require("@sentry/profiling-node");
 
 Sentry.init({
   dsn: process.env.SENTRY_URL,
-  integrations: [nodeProfilingIntegration()],
+  integrations: [
+    nodeProfilingIntegration(),
+  ],
+  // Tracing must be enabled for profiling to work
   tracesSampleRate: 1.0,
+  // Set sampling rate for profiling - this is evaluated only once per SDK.init call
   profileSessionSampleRate: 1.0,
+  // Trace lifecycle automatically enables profiling during active traces
   profileLifecycle: 'trace',
+
+  // Setting this option to true will send default PII data to Sentry.
+  // For example, automatic IP address collection on events
   sendDefaultPii: true,
 });
 
-Sentry.startSpan({ name: "Dummy app Span" }, () => {
-  console.log("Profiling active...");
+// Profiling happens automatically after setting it up with `Sentry.init()`.
+// All spans (unless those discarded by sampling) will have profiling data attached to them.
+Sentry.startSpan({
+  name: "Dummy Span",
+}, () => {
+  // The code executed here will be profiled
 });
 
-module.exports = Sentry;
